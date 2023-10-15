3e7733f9e8feacc5c7cd87e9d4e165fc42108950
Carlos_Garay <cgarayh@uni.pe>
2023-10-14 22:12:10 -0500

    create component

--- src/app/components/create-post/create-post.component.ts
+++ src/app/components/create-post/create-post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-post',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent {
+  image: File | undefined;
+  title: string = '';
+  body: string = '';
+
+  onFileSelected(event: any) {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      this.image = selectedFile;
+      console.log(this.image); // Aquí puedes trabajar con el archivo seleccionado
+    }
+  }
 
 }
